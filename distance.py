478a17301e4f83504ae070a48637d91ed9ffc50f
Nadim <nadimh5@hotmail.com>
2019-04-12 09:08:20 +0200

    f(y,x) skulle det vara, plot för jämförelse

--- distance.py
+++ distance.py
@@ -2,14 +2,16 @@ from __future__ import print_function
 
 import numpy as np
 from scipy.integrate import simps
+from scipy.integrate import cumtrapz
 from numpy import trapz
+import matplotlib.pyplot as plt
 
 f=open('ard.log',"r")
 lines=f.read().splitlines() 
 tid=[]
 wh1=[]
 wh2=[]
-interval = 0.1
+interval = 0
 d = 0.00
 for x in lines:
     n = float(x.split(' ')[0])
@@ -22,12 +24,23 @@ for x in lines:
     d = n
 f.close()
 
-dist_trapz1 = trapz(tid, wh1)
-dist_trapz2 = trapz(tid, wh2)
+#numpys trapz
+dist_trapz1 = trapz(wh1,tid)
+dist_trapz2 = trapz(wh2,tid)
 print("distance trapz integral wh1 =", dist_trapz1)
 print("distance trapz integral wh2 =", dist_trapz2)
 
-dist_simps1 = simps(tid, wh1)
-dist_simps2 = simps(tid, wh2)
+#scipys trapz
+dist_cumtrapz1 = cumtrapz(wh1, tid, initial=0)
+dist_cumtrapz2 = cumtrapz(wh2, tid, initial=0)
+print("distance cumtrapz integral wh1 =", dist_cumtrapz1)
+print("distance cumtrapz integral wh2 =", dist_cumtrapz2)
+
+#scipys simps
+dist_simps1 = simps(wh1, tid)
+dist_simps2 = simps(wh2, tid)
 print("distance simps integral wh1 =", dist_simps1)
 print("distance simps integral wh2 =", dist_simps2)
+
+plt.plot(tid, dist_cumtrapz1, 'r-', tid, dist_cumtrapz2, 'b-', tid, wh1, 'g-', tid, wh2, 'w-')
+plt.show()
